3fc380cafd8750f9c94d51fd3de0ca04eab11421
Santosh Yadav <santosh.yadav198613@gmail.com>
2023-04-21 22:59:21 +0200

    Initial commit

---
+++ src/env.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="@astrojs/image/client" />
+declare module 'hero-patterns'
+declare module 'lunr'
+declare module 'photoswipe-dynamic-caption-plugin'
