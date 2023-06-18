e80bc9c10b147d628eff4534900002429d47c623
Santosh Yadav <santosh.yadav198613@gmail.com>
2023-06-18 22:20:13 +0200

    add new blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,52 +1,41 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import image from '@astrojs/image'
-import mdx from '@astrojs/mdx'
-import alpinejs from '@astrojs/alpinejs'
-import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+import alpinejs from '@astrojs/alpinejs';
+import robotsTxt from 'astro-robots-txt';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import remarkPlantUML from '@akebifiky/remark-simple-plantuml';
+import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs';
+import { remarkDiagram } from './remark-plugins/remark-diagram.mjs';
 
-import remarkMath from 'remark-math'
-import rehypeKatex from 'rehype-katex'
-import remarkPlantUML from '@akebifiky/remark-simple-plantuml'
-import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs'
-import { remarkDiagram } from './remark-plugins/remark-diagram.mjs'
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     ssr: {
       external: ['svgo'],
-      noExternal: ['swiper', 'leaflet'],
-    },
+      noExternal: ['swiper', 'leaflet']
+    }
   },
   site: 'https://santoshyadav.dev',
   base: '/',
-  integrations: [
-    tailwind(),
-    sitemap(),
-    image(),
-    mdx(),
-    alpinejs(),
-    robotsTxt(),
-  ],
+  integrations: [tailwind(), sitemap(), image(), mdx(), alpinejs(), robotsTxt(), partytown()],
   experimental: {
-    integrations: true,
+    integrations: true
   },
   markdown: {
     extendDefaultPlugins: true,
-    remarkPlugins: [
-      remarkReadingTime,
-      remarkMath,
-      remarkPlantUML,
-      remarkDiagram,
-    ],
+    remarkPlugins: [remarkReadingTime, remarkMath, remarkPlantUML, remarkDiagram],
     rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: 'github-light',
       langs: [],
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
-  },
-})
+      wrap: true
+    }
+  }
+});
