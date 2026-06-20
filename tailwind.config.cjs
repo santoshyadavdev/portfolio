85380076e489edda136035a46d7e7d1b3bfe8704
Santosh Yadav <santosh@coderabbit.ai>
2026-06-20 04:01:34 +0200

    blog: angular 22 blog

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,25 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'h1 code, h2 code, h3 code, h4 code, h5 code, h6 code': {
+              backgroundColor: 'transparent',
+              padding: '0',
+              borderRadius: '0',
+              fontWeight: 'inherit',
+              fontSize: 'inherit',
+              fontFamily: 'inherit',
+              color: 'inherit',
+              '&::before': { content: 'none' },
+              '&::after': { content: 'none' },
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
